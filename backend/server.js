d4749e84c101fd93d5df91775e685f9087baf0bc
Praveen De Silva <desilvabkp.23@uom.lk>
2025-12-11 23:47:55 +0530

    refactor: organize middleware and route imports in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,17 @@ const cors = require('cors');
 
 const app = express();
 
+// >>> Middleware (allows us to read JSON data) <<<
 app.use(cors());
 app.use(express.json());
 
+// >>> Import routes <<<
 const resolveRoutes = require('./routes/resolveRoutes');
 
+// >>> Use routes <<<
 app.use('/api/resolve', resolveRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
 
